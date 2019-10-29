0ca9eefb09d74309867f5072d5250d59a63380f8
Khizar Saleem <saleemkhizar@outlook.com>
2019-10-29 10:59:41 -0600

    Added a MainViewModel class that took code out of MainActivity

--- app/src/main/java/edu/cnm/deepdive/diceware/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/diceware/controller/MainActivity.java
@@ -3,7 +3,9 @@ package edu.cnm.deepdive.diceware.controller;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,7 @@ import edu.cnm.deepdive.diceware.R;
 import edu.cnm.deepdive.diceware.service.DicewareService;
 import edu.cnm.deepdive.diceware.service.GoogleSignInService;
 import edu.cnm.deepdive.diceware.view.PassphraseAdapter;
+import edu.cnm.deepdive.diceware.viewmodel.MainViewModel;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
@@ -36,32 +39,26 @@ public class MainActivity extends AppCompatActivity {
       }
     });
     RecyclerView passphraseList = findViewById(R.id.keyword_list);
-    GoogleSignInService.getInstance().getAccount().observe(this, (account) -> {
-      if (account != null) {
-        String token = getString(R.string.oauth_header, account.getIdToken());
-        Log.d("Oauth2.0 token", token); // FIXME Remove before shipping.
-        DicewareService.getInstance().getAll(token)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe((passphrases) -> {
-              PassphraseAdapter adapter = new PassphraseAdapter(this, passphrases,
-                  (view, position, passphrase) -> {
-                    //TODO Add code to pop up editor.
-                    Log.d("Passphrase click", passphrase.getKey());
-                  },
-                  (menu, position, passphrase) -> {
-                    Log.d("Passphrase context", passphrase.getKey());
-                    getMenuInflater().inflate(R.menu.passphrase_context, menu);
-                    menu.findItem(R.id.delete_passphrase).setOnMenuItemClickListener(
-                        (item) -> {
-                          Log.d("Delete selected", passphrase.getKey());
-                          // TODO Send request to server to delete passphrase; refresh view.
-                          return true;
-                        });
-                  });
-              passphraseList.setAdapter(adapter);
-            });
-      }
+    MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+    GoogleSignInService.getInstance().getAccount().observe(this, (account) ->
+        viewModel.setAccount(account));
+    viewModel.getPassphrases().observe(this, (passphrases) -> {
+      PassphraseAdapter adapter = new PassphraseAdapter(this, passphrases,
+          (view, position, passphrase) -> {
+            //TODO Add code to pop up editor.
+            Log.d("Passphrase click", passphrase.getKey());
+          },
+          (menu, position, passphrase) -> {
+            Log.d("Passphrase context", passphrase.getKey());
+            getMenuInflater().inflate(R.menu.passphrase_context, menu);
+            menu.findItem(R.id.delete_passphrase).setOnMenuItemClickListener(
+                (item) -> {
+                  Log.d("Delete selected", passphrase.getKey());
+                  // TODO Send request to server to delete passphrase; refresh view.
+                  return true;
+                });
+          });
+      passphraseList.setAdapter(adapter);
     });
   }
 
